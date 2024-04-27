b1e0ddd6d402e436ae2ffc4b98d0ddf7be692cdf
Joshua Ward <ward.joshua92@yahoo.com>
2024-04-27 00:33:32 -0700

    Add: allow enter to repeat the last command

--- extensions/extension_parser.c
+++ extensions/extension_parser.c
@@ -1,9 +1,10 @@
 #include <string.h>
+//TODO: consider gperf for this
+//TODO: libgit automation to squash stuff so writes arent so dangerous in ed
 //TODO: calling this extensions is a bit confusing given the info file. consider "mods" or "extensions"
 //TODO: extract to config? this should be automated but if manually entered it should be extracted.
 #define  NUM_EXTENSIONS 1+1 //TODO: is this correct? could this be 1?
 
-//extern void get_filename (const char **const ibufpp);
 
 /* EXTENSION PROTOTPYES */
 void test_print (int *, int *);
@@ -26,9 +27,8 @@ static const enum Extension
 };
 
 /* PARSER */
-//TODO: ibufpp might be pass by value here, if so just pass by reference
+//TODO: do stuff with the global line_t buffer
 //Extensions should have argument format: inline int EXTENSION_NAME(const char ** const ibufpp, int *first_addr, int *second_addr, char* args)
-//TODO: pass in filename here
 int parse_extension (const char ** const ibufpp, const char* filename, int *first_addr, int *second_addr)
 {
   printf ("%s %i, %i \n", *ibufpp, *first_addr, *second_addr);	//TODO: remove after testing
@@ -37,6 +37,7 @@ int parse_extension (const char ** const ibufpp, const char* filename, int *firs
   int extension_index=0;
   enum Extension extension_select;
   for (int i=1; i<=NUM_EXTENSIONS; i++)
+	//TODO: resort extensions_s by recency
     {
 	if(strcmp(extensions_s[i-1],*ibufpp)==0){
 		extension_index=i;
@@ -57,7 +58,7 @@ int parse_extension (const char ** const ibufpp, const char* filename, int *firs
     default:
       printf ("extension not found..\n");
     }
-  return 0;			//TODO: error codes with breaks
+  return 0;			//TODO: error codes, also within cases
 }
 
 /* BEGIN EXTENSION DECLARATIONS */
@@ -69,14 +70,14 @@ inline void test_print(int *first_addr, int *second_addr){
 //uses ctags c library to list all funcs from first_addr to second_addr in ibufpp
 inline void funcs(const char ** const ibufpp, const char *filename, int *first_addr, int *second_addr){
    //char* ctags_extension = calloc(100);
-//     const char* filename = get_filename(ibufpp);
-printf("inside funcs\n");
+     printf("inside funcs\n");
     printf(filename);
 //TODO: calloc needs to be static bytes for ctags -x + dynamic filename
 //   strcat(ctags_extension, "ctags -x ");
    //ctags_extension -x $cur_filename
 }
 
+//TODO: grep for all files in current directory recursively, this happens often enough it should be a succint command.
 //TODO: //ask a chatbot via curl or local algorithm the given prompt, with context from first_addr to second_addr and insert the response into ibufpp at current address
 //NOTE: this may require an additional address is current address in main_loop or buffer.c is not preserved after address passing
 //TODO: ask(const char * ibufpp, int *first_addr, int *second_addr, char* prompt)
