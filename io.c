e9e5fa3ad24f5174dda75b6eeb6539d05b84e192
Joshua Ward <ward.joshua92@yahoo.com>
2024-05-02 18:43:06 -0700

    squishme: format with indent -npcs to make my kat happy

--- io.c
+++ io.c
@@ -26,29 +26,29 @@
 
 static const char *sharpie = "";
 void
-sharpie_alloc ()
+sharpie_alloc()
 {
-  sharpie = highlight_alloc_line ();
+  sharpie = highlight_alloc_line();
 }
 
 /* print text to stdout */
 //TODO: need to highlight_init in main.c
 //TODO: need to alloc, kat has bugz
 static void
-put_tty_line (const char *p, int len, const int gflags)
+put_tty_line(const char *p, int len, const int gflags)
 {
   const char escapes[] = "\a\b\f\n\r\t\v\\";	//" TODO: fix this escapo in kat
   const char escchars[] = "abfnrtv\\";	//"
   int col = 0;
   //TODO: make this leaner move this allocation into main_loop, probably just global space but try to be idiomatic with the ed project.
-  sharpie = highlight_line (p, sharpie, (size_t) len);
+  sharpie = highlight_line(p, sharpie, (size_t) len);
 
   if (!(gflags & GPR || gflags & GLS))
     {
-      printf ("%d\t", current_addr ());
+      printf("%d\t", current_addr());
       col = 8;
     }
-  puts (sharpie);
+  puts(sharpie);
 //TODO: read this, understand it and put back the l print feature for printing escape tokens
 //  while (--len >= 0)
 //    {
@@ -89,24 +89,24 @@ put_tty_line (const char *p, int len, const int gflags)
 
 /* print a range of lines to stdout */
 bool
-display_lines (int from, const int to, const int gflags)
+display_lines(int from, const int to, const int gflags)
 {
-  line_t *const ep = search_line_node (inc_addr (to));
-  line_t *bp = search_line_node (from);
+  line_t *const ep = search_line_node(inc_addr(to));
+  line_t *bp = search_line_node(from);
 
   if (!from)
     {
-      set_error_msg ("Invalid address");
+      set_error_msg("Invalid address");
       return false;
     }
   while (bp != ep)
     {
       //TODO: check for GPR and default conditions TODO test
-      const char *const s = get_sbuf_line (bp);
+      const char *const s = get_sbuf_line(bp);
       if (!s)
 	return false;
-      set_current_addr (from++);
-      put_tty_line (s, bp->len, gflags);
+      set_current_addr(from++);
+      put_tty_line(s, bp->len, gflags);
       bp = bp->q_forw;
     }
   return true;
@@ -115,7 +115,7 @@ display_lines (int from, const int to, const int gflags)
 
 /* return the parity of escapes at the end of a string */
 static bool
-trailing_escape (const char *const s, int len)
+trailing_escape(const char *const s, int len)
 {
   bool odd_escape = false;
   while (--len >= 0 && s[len] == '\\')
@@ -127,23 +127,23 @@ trailing_escape (const char *const s, int len)
 /* If *ibufpp contains an escaped newline, get an extended line (one
    with escaped newlines) from stdin */
 bool
-get_extended_line (const char **const ibufpp, int *const lenp,
-		   const bool strip_escaped_newlines)
+get_extended_line(const char **const ibufpp, int *const lenp,
+		  const bool strip_escaped_newlines)
 {
   static char *buf = 0;
   static int bufsz = 0;
   int len;
 
   for (len = 0; (*ibufpp)[len++] != '\n';);
-  if (len < 2 || !trailing_escape (*ibufpp, len - 1))
+  if (len < 2 || !trailing_escape(*ibufpp, len - 1))
     {
       if (lenp)
 	*lenp = len;
       return true;
     }
-  if (!resize_buffer (&buf, &bufsz, len))
+  if (!resize_buffer(&buf, &bufsz, len))
     return false;
-  memcpy (buf, *ibufpp, len);
+  memcpy(buf, *ibufpp, len);
   --len;
   buf[len - 1] = '\n';		/* strip trailing esc */
   if (strip_escaped_newlines)
@@ -151,26 +151,26 @@ get_extended_line (const char **const ibufpp, int *const lenp,
   while (true)
     {
       int len2;
-      const char *const s = get_tty_line (&len2);
+      const char *const s = get_tty_line(&len2);
       if (!s)
 	return false;
       if (len2 == 0 || s[len2 - 1] != '\n')
 	{
-	  set_error_msg ("Unexpected end-of-file");
+	  set_error_msg("Unexpected end-of-file");
 	  return false;
 	}
-      if (!resize_buffer (&buf, &bufsz, len + len2))
+      if (!resize_buffer(&buf, &bufsz, len + len2))
 	return false;
-      memcpy (buf + len, s, len2);
+      memcpy(buf + len, s, len2);
       len += len2;
-      if (len2 < 2 || !trailing_escape (buf, len - 1))
+      if (len2 < 2 || !trailing_escape(buf, len - 1))
 	break;
       --len;
       buf[len - 1] = '\n';	/* strip trailing esc */
       if (strip_escaped_newlines)
 	--len;			/* strip newline */
     }
-  if (!resize_buffer (&buf, &bufsz, len + 1))
+  if (!resize_buffer(&buf, &bufsz, len + 1))
     return false;
   buf[len] = 0;
   *ibufpp = buf;
@@ -184,7 +184,7 @@ get_extended_line (const char **const ibufpp, int *const lenp,
    Return pointer to buffer and line size (uncluding trailing newline
    if it exists) */
 const char *
-get_tty_line (int *const sizep)
+get_tty_line(int *const sizep)
 {
   static char *buf = 0;
   static int bufsz = 0;
@@ -192,21 +192,21 @@ get_tty_line (int *const sizep)
 
   while (true)
     {
-      const int c = getchar ();
+      const int c = getchar();
       if (c == EOF)
 	{
-	  if (ferror (stdin))
+	  if (ferror(stdin))
 	    {
-	      show_strerror ("stdin", errno);
-	      set_error_msg ("Cannot read stdin");
-	      clearerr (stdin);
+	      show_strerror("stdin", errno);
+	      set_error_msg("Cannot read stdin");
+	      clearerr(stdin);
 	      if (sizep)
 		*sizep = 0;
 	      return 0;
 	    }
 	  else
 	    {
-	      clearerr (stdin);
+	      clearerr(stdin);
 	      if (i != oi)
 		{
 		  oi = i;
@@ -221,7 +221,7 @@ get_tty_line (int *const sizep)
 	}
       else
 	{
-	  if (!resize_buffer (&buf, &bufsz, i + 2))
+	  if (!resize_buffer(&buf, &bufsz, i + 2))
 	    {
 	      if (sizep)
 		*sizep = 0;
@@ -229,7 +229,7 @@ get_tty_line (int *const sizep)
 	    }
 	  buf[i++] = c;
 	  if (!c)
-	    set_binary ();
+	    set_binary();
 	  if (c != '\n')
 	    continue;
 	  buf[i] = 0;
@@ -245,8 +245,8 @@ get_tty_line (int *const sizep)
    Return pointer to buffer and line size (uncluding trailing newline
    if it exists and is not added now) */
 static const char *
-read_stream_line (FILE *const fp, int *const sizep,
-		  bool *const newline_added_nowp)
+read_stream_line(FILE *const fp, int *const sizep,
+		 bool *const newline_added_nowp)
 {
   static char *buf = 0;
   static int bufsz = 0;
@@ -254,24 +254,24 @@ read_stream_line (FILE *const fp, int *const sizep,
 
   while (true)
     {
-      if (!resize_buffer (&buf, &bufsz, i + 2))
+      if (!resize_buffer(&buf, &bufsz, i + 2))
 	return 0;
-      c = getc (fp);
+      c = getc(fp);
       if (c == EOF)
 	break;
       buf[i++] = c;
       if (!c)
-	set_binary ();
+	set_binary();
       else if (c == '\n')
 	break;
     }
   buf[i] = 0;
   if (c == EOF)
     {
-      if (ferror (fp))
+      if (ferror(fp))
 	{
-	  show_strerror (0, errno);
-	  set_error_msg ("Cannot read input file");
+	  show_strerror(0, errno);
+	  set_error_msg("Cannot read input file");
 	  return 0;
 	}
       else if (i)
@@ -279,7 +279,7 @@ read_stream_line (FILE *const fp, int *const sizep,
 	  buf[i] = '\n';
 	  buf[i + 1] = 0;
 	  *newline_added_nowp = true;
-	  if (!isbinary ())
+	  if (!isbinary())
 	    ++i;
 	}
     }
@@ -290,30 +290,30 @@ read_stream_line (FILE *const fp, int *const sizep,
 
 /* read a stream into the editor buffer; return total size of data read */
 static long
-read_stream (FILE *const fp, const int addr)
+read_stream(FILE *const fp, const int addr)
 {
-  line_t *lp = search_line_node (addr);
+  line_t *lp = search_line_node(addr);
   undo_t *up = 0;
   long total_size = 0;
-  const bool o_isbinary = isbinary ();
-  const bool appended = (addr == last_addr ());
+  const bool o_isbinary = isbinary();
+  const bool appended = (addr == last_addr());
   bool newline_added_now = false;
 
-  set_current_addr (addr);
+  set_current_addr(addr);
   while (true)
     {
       int size = 0;
-      const char *const s = read_stream_line (fp, &size, &newline_added_now);
+      const char *const s = read_stream_line(fp, &size, &newline_added_now);
       if (!s)
 	return -1;
       if (size > 0)
 	total_size += size;
       else
 	break;
-      disable_interrupts ();
-      if (!put_sbuf_line (s, size + newline_added_now, current_addr ()))
+      disable_interrupts();
+      if (!put_sbuf_line(s, size + newline_added_now, current_addr()))
 	{
-	  enable_interrupts ();
+	  enable_interrupts();
 	  return -1;
 	}
       lp = lp->q_forw;
@@ -321,88 +321,88 @@ read_stream (FILE *const fp, const int addr)
 	up->tail = lp;
       else
 	{
-	  up = push_undo_atom (UADD, current_addr (), current_addr ());
+	  up = push_undo_atom(UADD, current_addr(), current_addr());
 	  if (!up)
 	    {
-	      enable_interrupts ();
+	      enable_interrupts();
 	      return -1;
 	    }
 	}
-      enable_interrupts ();
+      enable_interrupts();
     }
-  if (addr && appended && total_size && o_isbinary && newline_added ())
-    fputs ("Newline inserted\n", stderr);
-  else if (newline_added_now && (!appended || !isbinary ()))
-    fputs ("Newline appended\n", stderr);
-  if (isbinary () && !o_isbinary && newline_added_now && !appended)
+  if (addr && appended && total_size && o_isbinary && newline_added())
+    fputs("Newline inserted\n", stderr);
+  else if (newline_added_now && (!appended || !isbinary()))
+    fputs("Newline appended\n", stderr);
+  if (isbinary() && !o_isbinary && newline_added_now && !appended)
     ++total_size;
   if (!total_size)
     newline_added_now = true;
   if (appended && newline_added_now)
-    set_newline_added ();
+    set_newline_added();
   return total_size;
 }
 
 
 /* read a named file/pipe into the buffer; return line count */
 int
-read_file (const char *const filename, const int addr)
+read_file(const char *const filename, const int addr)
 {
   FILE *fp;
   long size;
   int ret;
 
   if (*filename == '!')
-    fp = popen (filename + 1, "r");
+    fp = popen(filename + 1, "r");
   else
-    fp = fopen (strip_escapes (filename), "r");
+    fp = fopen(strip_escapes(filename), "r");
   if (!fp)
     {
-      show_strerror (filename, errno);
-      set_error_msg ("Cannot open input file");
+      show_strerror(filename, errno);
+      set_error_msg("Cannot open input file");
       return -1;
     }
-  size = read_stream (fp, addr);
+  size = read_stream(fp, addr);
   if (size < 0)
     return -1;
   if (*filename == '!')
-    ret = pclose (fp);
+    ret = pclose(fp);
   else
-    ret = fclose (fp);
+    ret = fclose(fp);
   if (ret != 0)
     {
-      show_strerror (filename, errno);
-      set_error_msg ("Cannot close input file");
+      show_strerror(filename, errno);
+      set_error_msg("Cannot close input file");
       return -1;
     }
-  if (!scripted ())
-    fprintf (stderr, "%lu\n", size);
-  return current_addr () - addr;
+  if (!scripted())
+    fprintf(stderr, "%lu\n", size);
+  return current_addr() - addr;
 }
 
 
 /* write a range of lines to a stream */
 static long
-write_stream (FILE *const fp, int from, const int to)
+write_stream(FILE *const fp, int from, const int to)
 {
-  line_t *lp = search_line_node (from);
+  line_t *lp = search_line_node(from);
   long size = 0;
 
   while (from && from <= to)
     {
       int len;
-      char *p = get_sbuf_line (lp);
+      char *p = get_sbuf_line(lp);
       if (!p)
 	return -1;
       len = lp->len;
-      if (from != last_addr () || !isbinary () || !newline_added ())
+      if (from != last_addr() || !isbinary() || !newline_added())
 	p[len++] = '\n';
       size += len;
       while (--len >= 0)
-	if (fputc (*p++, fp) == EOF)
+	if (fputc(*p++, fp) == EOF)
 	  {
-	    show_strerror (0, errno);
-	    set_error_msg ("Cannot write file");
+	    show_strerror(0, errno);
+	    set_error_msg("Cannot write file");
 	    return -1;
 	  }
       ++from;
@@ -414,37 +414,37 @@ write_stream (FILE *const fp, int from, const int to)
 
 /* write a range of lines to a named file/pipe; return line count */
 int
-write_file (const char *const filename, const char *const mode,
-	    const int from, const int to)
+write_file(const char *const filename, const char *const mode,
+	   const int from, const int to)
 {
   FILE *fp;
   long size;
   int ret;
 
   if (*filename == '!')
-    fp = popen (filename + 1, "w");
+    fp = popen(filename + 1, "w");
   else
-    fp = fopen (strip_escapes (filename), mode);
+    fp = fopen(strip_escapes(filename), mode);
   if (!fp)
     {
-      show_strerror (filename, errno);
-      set_error_msg ("Cannot open output file");
+      show_strerror(filename, errno);
+      set_error_msg("Cannot open output file");
       return -1;
     }
-  size = write_stream (fp, from, to);
+  size = write_stream(fp, from, to);
   if (size < 0)
     return -1;
   if (*filename == '!')
-    ret = pclose (fp);
+    ret = pclose(fp);
   else
-    ret = fclose (fp);
+    ret = fclose(fp);
   if (ret != 0)
     {
-      show_strerror (filename, errno);
-      set_error_msg ("Cannot close output file");
+      show_strerror(filename, errno);
+      set_error_msg("Cannot close output file");
       return -1;
     }
-  if (!scripted ())
-    fprintf (stderr, "%lu\n", size);
+  if (!scripted())
+    fprintf(stderr, "%lu\n", size);
   return (from && from <= to) ? to - from + 1 : 0;
 }
