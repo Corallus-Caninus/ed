ceb39eda61220b2b55b87171dc5c3c09aa1f5034
Josh Ward <ward.joshua92@yahoo.com>
2025-02-14 23:41:56 -0800

    fix: Correct GPU IDs for training script.

--- pytorch/scribe/TRAIN_COMMAND.sh
+++ pytorch/scribe/TRAIN_COMMAND.sh
@@ -1,1 +1,1 @@
-nix-shell --run 'source ../AIVenv/bin/activate &&  PYTORCH_CUDA_ALLOC_CONF="expandable_segments:True" accelerate launch --gpu_ids "0,1" AI.py'
+nix-shell --run 'source ../AIVenv/bin/activate &&  PYTORCH_CUDA_ALLOC_CONF="expandable_segments:True" accelerate launch --gpu_ids "1,2" AI.py'
