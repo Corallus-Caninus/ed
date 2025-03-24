Fix: off by one in step function

pytorch/scribe/fbfgs/fbfgs.py
@@ -780,7 +780,7 @@ class FBFGS(Optimizer):
                 s_sparse = s.to_sparse().to(self.direction_device).to(self.direction_device) # Store s_sparse on direction_device
                 old_stps.append(s_sparse.coalesce().to(self.direction_device)) # NOTE: was cpu
                 ro.append(torch.tensor([(1.0 / ys)], device=self.direction_device)) # NOTE: was cpu #TODO: can we include information on convergence here. This may be an observation of the approximation accuracy. Also consider the alignment (gtd being as close to zero as possible). essentially we would be scaling how much the approximation is influenced by an entry based on its ability to converge.
-              if n_iter >= max_iter:
+              if n_iter > max_iter:
                 break
 #TODO: break here on n_iters
               # update scale of initial Hessian approximation