4e730d38867ce418233e39222e5f9bdb882c61a7
Josh Ward <ward.joshua92@yahoo.com>
2025-03-30 02:52:36 -0700

    refactor: Move data loading and tokenization outside closure

--- pytorch/scribe/AI.py
+++ pytorch/scribe/AI.py
@@ -82,6 +82,12 @@ step_count = 0
 dataset_size = 1000
 
 while True:
+  random_index = torch.randint(0, dataset_size, (1,)).item() # Generate a random index
+  batch_train = get_random_streaming_item(dataset, random_index)['code'] # Access data using random index
+
+  tokens = tokenizer(batch_train,truncation=True, max_length=200,padding=False, return_overflowing_tokens=False, return_length=True,return_tensors='pt').to("cuda")
+  input_ids, attention_mask = (tokens.input_ids, tokens.attention_mask)
+  print("got num_tokens: " + str(input_ids.size(1)))
   def closure(): # Define closure here, outside the if block
     total_loss= 0
     start_time = time.time()
@@ -90,7 +96,7 @@ while True:
     cache = None
     chunk_size=0 #1000
     grad_vector_size = 200 #5
-    num_tokens = input_ids.size(1)
+    num_tokens = len(tokens)
     num_steps = 0
     avg_loss = 0.
     if num_tokens == chunk_size+1:
@@ -136,12 +142,6 @@ while True:
     del outputs
     torch.cuda.empty_cache()
     return loss
-  random_index = torch.randint(0, dataset_size, (1,)).item() # Generate a random index
-  batch_train = get_random_streaming_item(dataset, random_index)['code'] # Access data using random index
-
-  tokens = tokenizer(batch_train,truncation=True, max_length=200,padding=False, return_overflowing_tokens=False, return_length=True,return_tensors='pt').to("cuda")
-  input_ids, attention_mask = (tokens.input_ids, tokens.attention_mask)
-  print("got num_tokens: " + str(input_ids.size(1)))
 
   print("-----------------------step---------------------")
   optimizer.step(closure)
