c582d64f44dc9cd5d69bc3f546c3734469328fac
Corallus-Caninus <ward.joshua92@yahoo.com>
2025-04-06 14:13:09 -0700

    chore: Comment out AI module import and run in Main.hs

--- pytorch/scribe/HAI/app/Main.hs
+++ pytorch/scribe/HAI/app/Main.hs
@@ -8,20 +8,4 @@ import           System.IO (hFlush, stdout)
 
 main :: IO ()
 main = do
-  --Debug.trace "Main: Starting main function..." $ return ()
-  --AI.runAI
-  --Debug.trace "Main: AI.runAI call finished." $ return ()
-  putStrLn "Starting Main.main"
-  hFlush stdout
-  aiMod <- importModule (pack "AI")
-  case aiMod of
-    Just ai_module -> do
-      putStrLn "AI module imported successfully."
-      runAIResult <- call ai_module (pack "runAI") []
-      case runAIResult of
-        Just result -> do
-          putStrLn "AI.runAI call finished successfully."
-          -- You might want to do something with the result here
-        Nothing -> putStrLn "Error calling AI.runAI."
-    Nothing -> putStrLn "Failed to import AI module."
   putStrLn "Finished Main.main"
