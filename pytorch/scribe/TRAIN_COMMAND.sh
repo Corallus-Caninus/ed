c7a2b1ca3113437a7e07650437ad83ece0f26b57
Corallus-Caninus <ward.joshua92@yahoo.com>
2025-04-08 02:41:16 -0700

    Chore: use local venv to this project (getting ready to create standalone repo)

--- pytorch/scribe/TRAIN_COMMAND.sh
+++ pytorch/scribe/TRAIN_COMMAND.sh
@@ -1,1 +1,1 @@
-nix-shell --run 'source ../AIVenv/bin/activate && export OMP_NUM_THREADS=12 && export TORCH_NUM_THREADS=12 && PYTORCH_CUDA_ALLOC_CONF="expandable_segments:True" accelerate launch --gpu_ids "0,1" AI.py'
+nix-shell --run 'source AIVenv/bin/activate && export OMP_NUM_THREADS=12 && export TORCH_NUM_THREADS=12 && PYTORCH_CUDA_ALLOC_CONF="expandable_segments:True" accelerate launch --gpu_ids "0,1" AI.py'
