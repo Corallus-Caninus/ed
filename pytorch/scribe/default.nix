851b5a21d1b5e17f12df1529949842c730ace132
Corallus-Caninus <ward.joshua92@yahoo.com>
2025-04-16 11:10:23 -0700

    Fix: ensure we use the cuda for k80 since we are standardizing on it as supported for affordability

--- pytorch/scribe/default.nix
+++ pytorch/scribe/default.nix
@@ -6,7 +6,7 @@ pkgs.mkShell {
   buildInputs = [
     pkgs.python39
     pkgs.pkg-config
-    pkgs.cudaPackages.cudatoolkit
+    pkgs.cudaPackages_11.cudatoolkit
     pkgs.linuxPackages.nvidia_x11_legacy470
 #    pkgs.glibc
 #    pkgs.glib
@@ -23,13 +23,15 @@ pkgs.mkShell {
 
   shellHook = ''
     echo "You are now using a NIX environment"
-    export CUDA_PATH=${pkgs.cudaPackages.cudatoolkit}
+    export CUDA_PATH=${pkgs.cudaPackages_11.cudatoolkit}
 #    export LD_LIBRARY_PATH=${pkgs.stdenv.cc.cc.lib}/lib
     export LD_LIBRARY_PATH=${pkgs.stdenv.cc.cc.lib}/lib:${pkgs.gcc12}/lib
 #    export LD_LIBRARY_PATH=${pkgs.gcc11}/lib
     export EXTRA_LD_FLAGS="-L\/lib -L${pkgs.linuxPackages.nvidia_x11_legacy470}\/lib"
-    export LD_LIBRARY_PATH=${pkgs.linuxPackages.nvidia_x11_legacy470}/lib:${pkgs.cudaPackages.cudatoolkit}/lib64:$LD_LIBRARY_PATH  
+#    export LD_LIBRARY_PATH=${pkgs.linuxPackages.nvidia_x11_legacy470}/lib:${pkgs.cudaPackages.cudatoolkit}/lib64:$LD_LIBRARY_PATH  
+    export LD_LIBRARY_PATH=${pkgs.linuxPackages.nvidia_x11_legacy470}/lib:$LD_LIBRARY_PATH
     export LD_LIBRARY_PATH=${pkgs.python39}/lib:$LD_LIBRARY_PATH
+    export LD_LIBRARY_PATH=$CUDA_PATH/lib:$LD_LIBRARY_PATH
 export PKG_CONFIG_PATH="${pkgs.python39}/lib/pkgconfig:$PKG_CONFIG_PATH"
     alias gcc="${pkgs.gcc12}/bin/gcc"
   '';
