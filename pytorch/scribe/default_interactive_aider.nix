a96d4bdc2597443ecec3be7f0461f76e20de79d0
Corallus-Caninus <ward.joshua92@yahoo.com>
2025-12-16 00:39:15 -0800

    Add: nix for python version to support the latest aider for interactive_aider.sh

---
+++ pytorch/scribe/default_interactive_aider.nix
@@ -0,0 +1,38 @@
+{ pkgs ? import <nixpkgs> { config.allowUnfree = true; config.nvidia.acceptLicense = true; } }:
+
+#TODO: how can we programatically install the virtual environment for the python dependencies?
+
+pkgs.mkShell {
+  buildInputs = [
+    pkgs.python312
+    pkgs.python312Packages.pip
+    pkgs.cudaPackages.cudatoolkit
+    pkgs.linuxPackages.nvidia_x11_legacy470
+#    pkgs.glibc
+#    pkgs.glib
+    pkgs.gcc12
+    pkgs.gcc-unwrapped
+    pkgs.ninja
+pkgs.libstdcxx5
+pkgs.zlib
+pkgs.bc
+#    pkgs.python3Packages.pytorchWithCuda
+#    pkgs.python3Packages.transformers
+#    pkgs.python3Packages.datasets
+#    pkgs.python3Packages.peft
+#    pkgs.python3Packages.pip
+  ];
+
+  shellHook = ''
+    echo "You are now using a NIX environment"
+    export CUDA_PATH=${pkgs.cudaPackages.cudatoolkit}
+#    export LD_LIBRARY_PATH=${pkgs.stdenv.cc.cc.lib}/lib
+    export LD_LIBRARY_PATH=${pkgs.stdenv.cc.cc.lib}/lib:${pkgs.gcc12}/lib
+#    export LD_LIBRARY_PATH=${pkgs.gcc11}/lib
+    export EXTRA_LD_FLAGS="-L\/lib -L${pkgs.linuxPackages.nvidia_x11_legacy470}\/lib"
+    export LD_LIBRARY_PATH=${pkgs.linuxPackages.nvidia_x11_legacy470}/lib:${pkgs.cudaPackages.cudatoolkit}/lib64:$LD_LIBRARY_PATH  
+    export LD_LIBRARY_PATH=${pkgs.zlib}/:$LD_LIBRARY_PATH  
+    export PATH=$PATH:/home/jward/.local/bin/
+    alias gcc="${pkgs.gcc12}/bin/gcc"
+  '';
+}
