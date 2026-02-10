c3250ec6f383298a75c39ff88bf5cfbfd1b527cf
Corallus-Caninus <ward.joshua92@yahoo.com>
2026-02-10 14:12:52 -0800

    squishme

--- pytorch/scribe/AI_test.py
+++ pytorch/scribe/AI_test.py
@@ -329,15 +329,16 @@ def closure():
                 reg_count += 1
 # TODO: TEST ME. NOTE: this is a false positive for negative orthogonality but we want GSO to hit warp drive on reduction
             if torch.dot(param.grad.view(-1), param.view(-1)).item() == 0:
-                reg_term = reg_term + 0.5
+# TODO: this is a constant. I dont think this ties it into the gradients right..
+                reg_term = reg_term + 0.5*torch.dot(param.grad.view(-1), param.grad.view(-1))/ torth.dot(param.grad.view(-1), param.grad.view(-1))
                 reg_count += 1
 #                reg_term = reg_term + torch.sqrt(torch.dot(param.grad.view(-1), param.grad.view(-1)).item())
 # TODO: orthogonal addition after event horizon regularizer
     # Create composite loss
 # NOTE: We perform the product here to resist the strong regularizer from overtaking the objective function
-    reg_term = reg_term / reg_count
     print("reg term: " + str(reg_term))
-    if reg_term > 0:
+    if reg_count > 0:
+        reg_term = reg_term / reg_count
         composite_loss =   reg_term* (total_loss_tensor**2)
     #    composite_loss =  1/50 * torch.tensor(reg_term, device=total_loss_tensor.device)# * total_loss_tensor
     #    composite_loss =  torch.tensor(composite_loss, device=total_loss_tensor.device) / total_loss_tensor
@@ -345,9 +346,10 @@ def closure():
     #    optimizer.zero_grad()
         # Perform second backward pass on composite loss
         composite_loss.backward()
-    print(f"Composite loss: " + str(composite_loss))
-#TODO: only graph the loss function not the regularizer too
-    return composite_loss.item()+ total_loss_tensor.item()
+        print(f"Composite loss: " + str(composite_loss))
+    #TODO: only graph the loss function not the regularizer too
+        return composite_loss.item()+ total_loss_tensor.item()
+    return  total_loss_tensor.item()
 # Main training loop
 while True:
     cache = None
